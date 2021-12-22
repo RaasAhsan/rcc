3c683a691fc000b13fbc5bcacb52f82642d44f65
Raas Ahsan <raas.ahsan@gmail.com>
2021-12-22 03:56:34 -0600

    register allocation for local variable declarations

--- modules/compiler/src/main/scala/com/raasahsan/rcc/Generator.scala
+++ modules/compiler/src/main/scala/com/raasahsan/rcc/Generator.scala
@@ -1,5 +1,7 @@
 package com.raasahsan.rcc
 
+import scala.collection.mutable
+
 object Generator {
 
   import Assembly._
@@ -35,10 +37,58 @@ object Generator {
       case _ => None
     }
 
-  def generateFunctionDefinition(fd: FunctionDefinition): Program = {
-    val declarations = findDeclarations(Statement.Compound(fd.statements))
-    val name = functionName(fd).get
+  private def directDeclaratorIdentifier(dd: DirectDeclarator): Option[Identifier] =
+    dd match {
+      case DirectDeclarator.Identifier(id) => Some(id)
+      case _ => None
+    }
 
+  def generateFunctionDefinition(fd: FunctionDefinition): Program = {
+    val name = functionName(fd).get
+    
+    // Perform register allocation and code generation at the same time
+    // Three-address code makes this much simpler, and perhaps makes
+    // it easier to decouple.
+    // TODO: RegisterAllocator class?
+
+    // TODO: Offset newtype
+
+    val symbols = new mutable.HashMap[String, Int]
+    var offset = 0
+
+    def nextOffset(size: Int): Int =
+      offset += size
+      offset
+
+    def allocateOrGet(name: String, size: Int): Int =
+      symbols.get(name) match {
+        case Some(offset) => offset
+        case None =>
+          val next = nextOffset(size)
+          symbols.put(name, next)
+          next
+      }
+
+    // Allocate storage on the stack for all local variables, including nested compounds
+    val declarations = findDeclarations(Statement.Compound(fd.statements))
+    val names = declarations.flatMap { decl =>
+      decl.initDeclaratorList.get.declarators.toList.map { initDecl =>
+        // TODO: handle pointers, typing here
+        directDeclaratorIdentifier(initDecl.declarator.directDeclarator).get
+      }
+    }
+
+    names.foreach { ident =>
+      // handle pointers, typing here
+      allocateOrGet(ident.value, 4)  
+    }
+
+    // Next, evaluate all initializers and statements, allocating storage on stack if necessary
+    def generateStatement(statement: Statement): Unit = {
+      
+    }
+    
+    // TODO: Boilerplate for lines
     val lines = List(
       Label(name.value).line,
       Instruction.Push(Register.rbp.operand).line,
